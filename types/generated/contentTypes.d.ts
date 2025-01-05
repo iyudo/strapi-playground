4b1a377c34c930417f0c34712aa42b0daa3a06a1
iyudo <ivan.yudo@traveloka.com>
2025-01-05 17:16:51 +0700

    add new article

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -462,6 +462,35 @@ export interface ApiHotelArticleHotelArticle
   };
 }
 
+export interface ApiNewArticleNewArticle extends Struct.CollectionTypeSchema {
+  collectionName: 'new_articles';
+  info: {
+    displayName: 'NewArticle';
+    pluralName: 'new-articles';
+    singularName: 'new-article';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::new-article.new-article'
+    > &
+      Schema.Attribute.Private;
+    publishedAt: Schema.Attribute.DateTime;
+    Rank: Schema.Attribute.Integer;
+    Title: Schema.Attribute.String;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -974,6 +1003,7 @@ declare module '@strapi/strapi' {
       'api::article.article': ApiArticleArticle;
       'api::flight-article.flight-article': ApiFlightArticleFlightArticle;
       'api::hotel-article.hotel-article': ApiHotelArticleHotelArticle;
+      'api::new-article.new-article': ApiNewArticleNewArticle;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
