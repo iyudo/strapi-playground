08b1e56514869b697146140f43560461faf12221
iyudo <ivan.yudo@traveloka.com>
2025-01-23 15:12:17 +0700

    add specific url

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,6 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('PUBLIC_URL', 'https://cxpcms-lbext-cbf157041239fec2-695940355.ap-southeast-1.elb.amazonaws.com'),
+  proxy: env.bool('IS_PROXIED', true),
 });
